87cdb6a71c6a0285d743a5fe59fbecf07e9de8b1
Tabata9 <tabatarsouza@gmail.com>
2023-12-16 18:13:58 -0300

    Desafio final ok

---
+++ classes_de_herois.js
@@ -0,0 +1,34 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque;
+  
+      switch (this.tipo) {
+        case "mago":
+          ataque = "usou magia";
+          break;
+        case "guerreiro":
+          ataque = "usou espada";
+          break;
+        case "monge":
+          ataque = "usou artes marciais";
+          break;
+        case "ninja":
+          ataque = "usou shuriken";
+          break;
+        default:
+          ataque = "realizou um ataque";
+      }
+  
+      console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+  }
+  
+  const meuHeroi = new Heroi("Aragorn", 30, "guerreiro");
+  meuHeroi.atacar(); 
+  
